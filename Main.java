6c3ec1da530cfce4d472e56fcf4ac887d2139ced
Prakash Dahal <prakash@innovationhub-act.org>
2023-06-05 16:26:47 -0400

    database sql command:

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args)
+  {
+  String name = "this is my country";
+  float a = 4.5f;
+    System.out.println("this is testing purpose");
+    System.out.println(name); 
+    System.out.println(name);
+    System.out.println(name);
+    
+  }
+  }
